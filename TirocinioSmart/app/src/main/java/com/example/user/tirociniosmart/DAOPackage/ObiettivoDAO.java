bbb7300d407f92ef52099a81397c758df7388f8f
Raiser <r.serpe2@studenti.unisa.it>
2018-01-22 15:51:23 +0100

    Aggiunto metodi a ObiettivoDAO

--- TirocinioSmart/app/src/main/java/com/example/user/tirociniosmart/DAOPackage/ObiettivoDAO.java
+++ TirocinioSmart/app/src/main/java/com/example/user/tirociniosmart/DAOPackage/ObiettivoDAO.java
@@ -1,5 +1,12 @@
 package com.example.user.tirociniosmart.DAOPackage;
 
+import com.example.user.tirociniosmart.EntityPackage.Obiettivo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by User on 17/01/2018.
  */
@@ -11,10 +18,73 @@ public class ObiettivoDAO extends GenericDAO {
         genericConnectionPool = connectionPool;
     }
 
+    public static String insert(Obiettivo obiettivo) {
+        Connection newConnection = null;
+        try {
+            newConnection = (Connection) genericConnectionPool.getConnection();
+            newConnection.setAutoCommit(false);
 
-    public static String insert() {
-        return "";
+            System.out.println("Database connesso");
+            PreparedStatement stt = null;
+            if(ObiettivoDAO.checkObiettivo(obiettivo)) {
+                stt = newConnection.prepareStatement("INSERT INTO Obiettivo_formativo (Nome,Descrizione,AziendaID) VALUES (?,?,?)");
+                stt.setString(1,obiettivo.getNome());
+                stt.setString(2,obiettivo.getDescrizione());
+                stt.setString(3,obiettivo.getAzienda().getId());
+                stt.executeUpdate();
+
+                newConnection.commit();
+                stt.close();
+
+                genericConnectionPool.releaseConnection(newConnection);
+
+                return "Inserimento avvenuto correttamente";
+            } else
+                return "Esiste già un obiettivo con questo nome";
+        } catch (SQLException e) {
+            return "Connessione al database non presente";
+        }
     }
+
+    public static boolean checkObiettivo(Obiettivo obiettivo) throws SQLException {
+        Connection newConnection = (Connection) genericConnectionPool.getConnection();
+        newConnection.setAutoCommit(false);
+        System.out.println("Database connesso");
+        PreparedStatement stt = newConnection.prepareStatement("SELECT * FROM Obiettivo WHERE Nome = ?");
+        stt.setString(1,obiettivo.getNome());
+        ResultSet rs=stt.executeQuery();
+        if(rs.next())
+            return false;
+        else
+            return true;
+    }
+
+    public static String remove(Obiettivo obiettivo) {
+        Connection newConnection = null;
+        try {
+            newConnection = (Connection) genericConnectionPool.getConnection();
+            newConnection.setAutoCommit(false);
+
+            System.out.println("Database connesso");
+            PreparedStatement stt = null;
+            if(!ObiettivoDAO.checkObiettivo(obiettivo)) {
+                stt = newConnection.prepareStatement("DELETE FROM Obiettivo_formativo WHERE Nome = ?");
+                stt.setString(1,obiettivo.getNome());
+                stt.executeUpdate();
+
+                newConnection.commit();
+                stt.close();
+
+                genericConnectionPool.releaseConnection(newConnection);
+
+                return "Rimozione avvenuta correttamente";
+            } else
+                return "Non esiste un obiettivo con questo nome";
+        } catch (SQLException e) {
+            return "Connessione al database non presente";
+        }
+    }
+
 
     public static String update() {
         return "";
