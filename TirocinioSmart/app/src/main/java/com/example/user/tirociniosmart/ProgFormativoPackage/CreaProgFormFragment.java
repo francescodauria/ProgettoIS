410392bdd687b9844fcbb07e646fefe741507449
Francesco <f.dauria37@studenti.unisa.it>
2018-01-24 17:39:56 +0100

    Aggiunta calendar creazione progetto formativo

--- TirocinioSmart/app/src/main/java/com/example/user/tirociniosmart/ProgFormativoPackage/CreaProgFormFragment.java
+++ TirocinioSmart/app/src/main/java/com/example/user/tirociniosmart/ProgFormativoPackage/CreaProgFormFragment.java
@@ -1,6 +1,7 @@
 package com.example.user.tirociniosmart.ProgFormativoPackage;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
@@ -10,22 +11,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.user.tirociniosmart.R;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by User on 29/10/2017.
  */
 
-public class CreaProgFormFragment extends Fragment {
+public class CreaProgFormFragment extends Fragment implements View.OnClickListener, DatePickerDialog.OnDateSetListener{
 
     private View view;
     private FragmentManager fm;
     private Button signature;
     private ImageView imageFirma;
+    private TextView data_inizio;
+    private TextView data_fine;
+    private Date data_I;
+    private Date data_F;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
 
         view = inflater.inflate(R.layout.student_fragment_nuova_richiesta_layout, container, false);
@@ -45,8 +61,14 @@ public class CreaProgFormFragment extends Fragment {
 
 
         });
-
-
+        Spinner azienda=(Spinner) view.findViewById(R.id.spinnerAzienda);
+        List<String> aziende=new ArrayList<>();
+        aziende.add("ITSYSTEM");
+        azienda.setAdapter(new ArrayAdapter<String>(getActivity(),R.layout.support_simple_spinner_dropdown_item,aziende));
+        data_inizio=view.findViewById(R.id.data_inizio);
+        data_fine=view.findViewById(R.id.data_fine);
+        data_inizio.setOnClickListener(this);
+        data_fine.setOnClickListener(this);
         return view;
 
     }
@@ -71,5 +93,41 @@ public class CreaProgFormFragment extends Fragment {
             }
         }
 
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        if(data_inizio.getTag().equals("click"))
+        {
+            String data=""+i2+"-"+i1+"-"+i;
+            data_I=new Date(i,i1,i2);
+            data_inizio.setText(data);
+            data_inizio.setTag("");
+        }
+        else if (data_fine.getTag().equals("click"))
+        {
+            String data=""+i2+"-"+i1+"-"+i;
+            data_F=new Date(i,i1,i2);
+            data_fine.setText(data);
+            data_fine.setTag("");
+        }
+        else ;
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView text=(TextView)view;
+        if(text.getId()==data_inizio.getId())
+        {
+            data_inizio.setTag("click");
+            System.out.println("Click data_inizio");
+        }
+        else if(text.getId()==data_fine.getId())
+        {
+            data_fine.setTag("click");
+            System.out.println("Click data_fine");
+        }
+        DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, 2018, 1, 1);
+        dialog.show();
     }
 }
